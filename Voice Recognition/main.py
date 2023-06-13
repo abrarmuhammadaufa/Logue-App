02fd0d16c99d3826bebac548149029fef2e6cba5
asepzayn <82562533+asepzayn@users.noreply.github.com>
2023-06-13 18:40:06 +0700

    Update main.py
    deploy with flask, add CTCLoss function, add predict function

--- Voice Recognition/main.py
+++ Voice Recognition/main.py
@@ -1,21 +1,43 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras.models import load_model
-from utils import preprocess_audio, predict_from_file, CTCLoss
-import sounddevice as sd
-import soundfile as sf
+from utils import preprocess_audio, predict_from_file, decode_batch_predictions
+from flask import Flask, jsonify, request
 
-samplerate = 16000  
-duration = 6 # seconds
-filename = 'test_1.wav'
-print("start")
-mydata = sd.rec(int(samplerate * duration), samplerate=samplerate,
-    channels=1, blocking=True)
-print("end")
-sd.wait()
-sf.write(filename, mydata, samplerate)
+app = Flask(__name__)
+
+@tf.function
+def CTCLoss(y_true, y_pred):
+    batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
+    input_length = tf.cast(tf.shape(y_pred)[1], dtype="int64")
+    label_length = tf.cast(tf.shape(y_true)[1], dtype="int64")
+    
+    input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+    label_length = label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+    
+    loss = keras.backend.ctc_batch_cost(y_true, y_pred, input_length, label_length)
+
+    return loss
 
 
-loaded_model = load_model("model.h5", custom_objects={'CTCLoss': CTCLoss})
+with keras.utils.custom_object_scope({'CTCLoss': CTCLoss}):
+    model = keras.models.load_model('model.h5')
 
-file_path = filename  # Replace with the path to your audio file
-predictions = predict_from_file(file_path, loaded_model)
-print("Predictions:", predictions)
+@app.route("/predict", methods=["POST"])
+def predict():
+    file = request.files['file']
+    if 'file' not in request.files or file.filename == "":
+        return jsonify({'error': 'No file uploaded'})
+
+    try:
+        preprocessed_audio = preprocess_audio(file)
+        preprocessed_audio = np.expand_dims(preprocessed_audio, axis=0)
+        predictions = model.predict(preprocessed_audio)
+        decoded_predictions = decode_batch_predictions(predictions)[0]
+        return jsonify({"predictions": decoded_predictions})
+    except:
+        return jsonify({'error': str(e)})
+
+if __name__ == "__main__":
+    app.run()
