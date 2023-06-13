bd34acd05902639477192ad371aacd09b111613c
asepzayn <82562533+asepzayn@users.noreply.github.com>
2023-06-13 18:45:21 +0700

    Update main.py
    move CTCLoss function to utils.py

--- Voice Recognition/main.py
+++ Voice Recognition/main.py
@@ -2,24 +2,10 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import load_model
-from utils import preprocess_audio, predict_from_file, decode_batch_predictions
+from utils import preprocess_audio, decode_batch_predictions, CTCLoss
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-
-@tf.function
-def CTCLoss(y_true, y_pred):
-    batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
-    input_length = tf.cast(tf.shape(y_pred)[1], dtype="int64")
-    label_length = tf.cast(tf.shape(y_true)[1], dtype="int64")
-    
-    input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
-    label_length = label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
-    
-    loss = keras.backend.ctc_batch_cost(y_true, y_pred, input_length, label_length)
-
-    return loss
-
 
 with keras.utils.custom_object_scope({'CTCLoss': CTCLoss}):
     model = keras.models.load_model('model.h5')
